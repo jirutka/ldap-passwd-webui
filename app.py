d24ffe217d84a17a1f7766500cb89b7b94781b31
Jakub Jirutka <jakub@jirutka.cz>
2017-07-14 01:22:11 +0200

    Fix code-style

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ LOG.info("Starting ldap-passwd-webui %s" % VERSION)
 
 CONF = read_config()
 
-bottle.TEMPLATE_PATH = [ BASE_DIR ]
+bottle.TEMPLATE_PATH = [BASE_DIR]
 
 # Set default attributes to pass into templates.
 SimpleTemplate.defaults = dict(CONF['html'])
